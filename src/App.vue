be8e12ddf13941e23b1ba5b51149e080ecd33671
Levi Frota <gideao.frota@enext.com.br>
2023-04-14 14:16:53 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to your Vue.js App" @button-click="onButtonClick" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    onButtonClick() {
+      console.log("clicado");
+    },
+  },
+};
 </script>
 
 <style>
